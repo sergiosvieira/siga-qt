fbdde9699849dfa02afa6962057b31d358b45c03
Sergio Vieira <sergiosvieira@gmail.com>
2016-07-12 16:55:24 -0300

    Adds news tests in TimeSerieTree

--- test/time-serie-tree/time-serie-tests.cpp
+++ test/time-serie-tree/time-serie-tests.cpp
@@ -9,21 +9,50 @@ using namespace SIGA::DS;
 
 Describe (TimeSerieTreeClassTest)
 {
-    It(InsertValueInYearlyTreeTest)
+    It(InsertValueInYearlyTimeSerieTree)
     {
         TimeSerieTree tree(1978, PeriodType::YEARLY_TYPE);
         int stationID = 1;
         CDate date{1978, 01, 31};
         float value = 10.f;
-        tree.insertStationID(stationID);
-        tree.insertValueOnStation(date, stationID, value);
-        tuple<float, float, float> result = tree.valueOnStation(date, stationID);
+        tree.setDateValue(date, stationID, value);
+        tuple<float, float, float> result = tree.dateValues(date, stationID);
         // Day
-        Assert::That(std::get<0>(result), Equals(value));
+        Assert::That(std::get<0>(result), Is().EqualToWithDelta(0.0273973, 0.001));
         // Month
-        Assert::That(std::get<1>(result), Equals(value));
+        Assert::That(std::get<1>(result), Is().EqualToWithDelta(0.833f, 0.001));
         // Year
-        Assert::That(std::get<2>(result), Equals(value));
+        Assert::That(std::get<2>(result), Equals(10.f));
+    }
+    It(InsertValueInMonthlyTimeSerieTree)
+    {
+        TimeSerieTree tree(1978, PeriodType::MONTHLY_TYPE);
+        int stationID = 1;
+        CDate date{1978, 01, 31};
+        float value = 10.f;
+        tree.setDateValue(date, stationID, value);
+        tuple<float, float, float> result = tree.dateValues(date, stationID);
+        // Day
+        Assert::That(tree.dayValue(date, stationID), Is().EqualToWithDelta(0.3225f, 0.001));
+        // Month
+        Assert::That(tree.monthValue(date, stationID), Is().EqualToWithDelta(10.f, 0.001));
+        // Year
+        Assert::That(tree.yearValue(date, stationID), Is().EqualToWithDelta(10.f, 0.001));
+    }
+    It(InsertValueInDailyTimeSerieTree)
+    {
+        TimeSerieTree tree(1978, PeriodType::DAILY_TYPE);
+        int stationID = 1;
+        CDate date{1978, 01, 31};
+        float value = 10.f;
+        tree.setDateValue(date, stationID, value);
+        tuple<float, float, float> result = tree.dateValues(date, stationID);
+        // Day
+        Assert::That(tree.dayValue(date, stationID), Equals(10.f));
+        // Month
+        Assert::That(tree.monthValue(date, stationID), Equals(10.f));
+        // Year
+        Assert::That(tree.yearValue(date, stationID), Equals(10.f));
     }
 };
 
